61cf1ed7523faa031357f4c782287f392f8198dd
GhostMobin <s1810677110@ru.ac.bd>
2024-01-07 16:23:56 +0600

    data form updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
     res.send('Quality Cravings Server Running...')
 })
@@ -22,6 +23,7 @@ mongoose.connect(process.env.DB_URI)
 
 // Schemas
 const brandSchema = new mongoose.Schema({
+    // _id: mongoose.Schema.Types.ObjectId,
     name: String,
     imageUrl: String,
     advertisementImages: [String]
@@ -62,76 +64,148 @@ const Testimonial = mongoose.model('Testimonial', testimonialSchema);
 
 // Brand CRUD
 app.get('/brands', async (req, res) => {
-    const brands = await Brand.find();
-    res.send(brands);
+    try {
+        const brands = await Brand.find();
+        res.send(brands);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.get('/brands/:brandId', async (req, res) => {
-    const id = req.params.brandId;
-    const brand = await Brand.findById(id);
-    res.send(brand);
+    try {
+        const id = req.params.brandId;
+        const brand = await Brand.findById(id);
+        res.send(brand);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.get('/brands/:brandId/products', async (req, res) => {
-    const brandId = req.params.brandId;
-    const products = await Product.find({ brandId });
-    res.send(products);
+    try {
+        const brandId = req.params.brandId;
+        const products = await Product.find({ brandId });
+        res.send(products);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 // Product CRUD
 app.get('/products', async (req, res) => {
-    const products = await Product.find();
-    res.send(products);
+    try {
+        const products = await Product.find();
+        res.send(products);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.post('/products', async (req, res) => {
-    const newProduct = req.body;
-    const product = await Product.create(newProduct);
-    res.json(product);
+    try {
+        const newProduct = req.body;
+        const product = await Product.create(newProduct);
+        res.json(product);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.get('/products/:productId', async (req, res) => {
-    const id = req.params.productId;
-    const product = await Product.findById(id);
-    res.send(product);
+    try {
+        const id = req.params.productId;
+        const product = await Product.findById(id);
+        res.send(product);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.put('/products/:productId', async (req, res) => {
-    const id = req.params.productId;
-    const updateProduct = req.body;
-    const product = await Product.findByIdAndUpdate(id, updateProduct, { new: true });
-    res.send(product);
+    try {
+        const id = req.params.productId;
+        const updateProduct = req.body;
+        const product = await Product.findByIdAndUpdate(id, updateProduct, { new: true });
+        res.send(product);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 // User CRUD
 app.get('/users', async (req, res) => {
-    const users = await User.find();
-    res.send(users);
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.get('/users/:userEmail', async (req, res) => {
-    const userEmail = req.params.userEmail;
-    const user = await User.findOne({ email: userEmail });
-    res.send(user);
+    try {
+        const userEmail = req.params.userEmail;
+        const user = await User.findOne({ email: userEmail });
+        res.send(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.post('/users', async (req, res) => {
-    const newUser = req.body;
-    const user = await User.create(newUser);
-    res.json(user);
+    try {
+        const newUser = req.body;
+        const user = await User.create(newUser);
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.put('/users/:userEmail', async (req, res) => {
-    const userEmail = req.params.userEmail;
-    const { cartItems } = req.body;
-    const user = await User.findOneAndUpdate({ email: userEmail }, { cartItems }, { new: true });
-    res.send(user);
+    try {
+        const userEmail = req.params.userEmail;
+        const { cartItems } = req.body;
+        const user = await User.findOneAndUpdate({ email: userEmail }, { cartItems }, { new: true });
+        res.send(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 // Testimonial CRUD
 app.get('/testimonials', async (req, res) => {
-    const testimonials = await Testimonial.find();
-    res.send(testimonials);
+    try {
+        const testimonials = await Testimonial.find();
+        res.send(testimonials);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).send({ error: err.message });
+    }
+
 });
 
 app.listen(port, () => {
