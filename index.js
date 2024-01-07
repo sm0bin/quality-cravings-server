cef3fbfaefe87cf7538df543ef575d1dbc17a247
GhostMobin <s1810677110@ru.ac.bd>
2024-01-07 16:29:35 +0600

    clear code

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ mongoose.connect(process.env.DB_URI)
 
 // Schemas
 const brandSchema = new mongoose.Schema({
-    // _id: mongoose.Schema.Types.ObjectId,
     name: String,
     imageUrl: String,
     advertisementImages: [String]
